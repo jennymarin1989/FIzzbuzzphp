d76089d0b0435de5f17e4ff6aa0b9cda02c126bd
jose <jennymarin1989@hotmail.com>
2018-04-19 15:16:31 +0100

    Add test for fizz function, return fizz when passing a multiple of 3

---
+++ src/Fizzbuzz.php
@@ -0,0 +1,11 @@
+<?php
+
+    class Fizzbuzz
+    {
+        public function fizz($a)
+        {
+           if($a %3 ==0){
+               return "fizz";
+           }
+        }
+    }
---
+++ test/FizzbuzzTest.php
@@ -0,0 +1,23 @@
+<?php
+
+    include_once(dirname(__FILE__) . '../../src/Fizzbuzz.php');
+    
+
+    class FizzbuzzTests extends PHPunit\Framework\Testcase
+    
+    {
+        private $fizzbuzz;
+
+        protected function setUp()
+        {
+            $this->fizzbuzz = new Fizzbuzz();
+        }
+
+        public function testFizz()
+        {
+            $result = $this->fizzbuzz->fizz(3);
+            $this->assertEquals("fizz", $result);
+        }
+
+
+    }
